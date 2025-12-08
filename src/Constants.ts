2beac83e8993a6f232b341960c1e78f08c0babfe
יחיאל <yehieldimenshtein@gmail.com>
2025-12-09 00:06:04 +0200

    ENV to client side

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,1 +1,5 @@
-export const HOST = "https://project-2026.onrender.com/";
+export let HOST: any;
+// @ts-ignore
+HOST = import.meta.env.VITE_HOST ?? "https://project-2026.onrender.com/";
+//http://localhost:8080/   local
+//https://project-2026.onrender.com/
