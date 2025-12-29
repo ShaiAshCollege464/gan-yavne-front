e9a0c9f388fe45bd724ac37e8abf3f75307ef6c4
יחיאל <yehieldimenshtein@gmail.com>
2025-12-29 17:55:03 +0200

    signup.2

--- src/SignupPage.jsx
+++ src/SignupPage.jsx
@@ -1,130 +1,175 @@
-import {useNavigate} from "react-router-dom";
-import {useState} from "react";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import axios from "axios";
-import {HOST} from "./Constants.js";
+import { HOST } from "./Constants.js";
 import Cookies from "js-cookie";
 
 const USERS_TYPES = {
     NONE: 0,
     CLIENT: 1,
     PROFESSIONALIST: 2
-}
+};
 
 const SIGNUP_STATUSES = {
     PENDING: 0,
     SUCCESS: 1,
     FAILURE: 2
-}
+};
 
 function SignupPage() {
     const navigate = useNavigate();
+
     const [selectedType, setSelectedType] = useState(USERS_TYPES.NONE);
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [contactInfo, setContactInfo] = useState("");
     const [fullName, setFullName] = useState("");
-    const [address, setAddress] = useState("")
-
-    const [areas, setAreas] = useState("")
-    const [plan, setPlan] = useState("")
+    const [address, setAddress] = useState("");
+    const [areas, setAreas] = useState("");
+    const [plan, setPlan] = useState(0);
 
     const [signupStatus, setSignupStatus] = useState(SIGNUP_STATUSES.PENDING);
 
     return (
         <>
-            {
-                signupStatus === SIGNUP_STATUSES.FAILURE &&
-                <div>
-                    Something Wrong...
-                </div>
-            }
+            {signupStatus === SIGNUP_STATUSES.FAILURE && (
+                <div>Something Wrong...</div>
+            )}
 
-            <select value={selectedType} onChange={(event) => {
-                setSelectedType(event.target.value);
-            }}>
-                <option disabled={true} value={USERS_TYPES.NONE}>
+            <select
+                value={selectedType}
+                onChange={(event) => {
+                    setSelectedType(Number(event.target.value));
+                }}
+            >
+                <option disabled value={USERS_TYPES.NONE}>
                     Select type
                 </option>
-                <option value={USERS_TYPES.CLIENT}>
-                    Client
-                </option>
+                <option value={USERS_TYPES.CLIENT}>Client</option>
                 <option value={USERS_TYPES.PROFESSIONALIST}>
                     Professionalist
                 </option>
             </select>
-            {
-                selectedType !== USERS_TYPES.NONE &&
+
+            {selectedType !== USERS_TYPES.NONE && (
                 <>
                     <div>
-                        <input placeholder={"username"} type={"text"} value={username} onChange={(event) => {
-                            setUsername(event.target.value);
-                        }}/>
+                        <input
+                            placeholder="username"
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
                     </div>
+
                     <div>
-                        <input placeholder={"password"} type={"password"} value={password} onChange={(event) => {
-                            setPassword(event.target.value);
-                        }}/>
+                        <input
+                            placeholder="password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
                     </div>
+
                     <div>
-                        <input placeholder={"contactInfo"} type={"text"} value={contactInfo} onChange={(event) => {
-                            setContactInfo(event.target.value);
-                        }}/>
+                        <input
+                            placeholder="contactInfo"
+                            type="text"
+                            value={contactInfo}
+                            onChange={(e) => setContactInfo(e.target.value)}
+                        />
                     </div>
+
                     <div>
-                        <input placeholder={"full name"} type={"text"} value={fullName} onChange={(event) => {
-                            setFullName(event.target.value);
-                        }}/>
+                        <input
+                            placeholder="full name"
+                            type="text"
+                            value={fullName}
+                            onChange={(e) => setFullName(e.target.value)}
+                        />
                     </div>
-                    {
-                        selectedType === USERS_TYPES.CLIENT &&
+
+                    {selectedType === USERS_TYPES.CLIENT && (
                         <div>
-                            <input placeholder={"address"} type={"text"} value={address} onChange={(event) => {
-                                setAddress(event.target.value);
-                            }}/>
+                            <input
+                                placeholder="address"
+                                type="text"
+                                value={address}
+                                onChange={(e) => setAddress(e.target.value)}
+                            />
                         </div>
-                    }
-                    {
-                        selectedType === USERS_TYPES.PROFESSIONALIST &&
+                    )}
+
+                    {selectedType === USERS_TYPES.PROFESSIONALIST && (
                         <>
                             <div>
-                                <input placeholder={"areas"} type={"text"} value={areas} onChange={(event) => {
-                                    setAreas(event.target.value);
-                                }}/>
+                                <input
+                                    placeholder="areas"
+                                    type="text"
+                                    value={areas}
+                                    onChange={(e) => setAreas(e.target.value)}
+                                />
                             </div>
+
                             <div>
-                                <input placeholder={"plan"} type={"text"} value={plan} onChange={(event) => {
-                                    setPlan(event.target.value);
-                                }}/>
+                                <input
+                                    placeholder="plan"
+                                    type="number"
+                                    value={plan}
+                                    onChange={(e) =>
+                                        setPlan(Number(e.target.value))
+                                    }
+                                />
                             </div>
                         </>
-                    }
-                </>}
+                    )}
+                </>
+            )}
 
             <div>
                 <button
                     disabled={
-                        username.length === 0
-                        || password.length !== 6
-                        || selectedType === USERS_TYPES.NONE
-                        || contactInfo.length === 0
-                        || !fullName.includes(" ")
-                        || ((selectedType === USERS_TYPES.CLIENT) && address.length === 0)
-                        || ((selectedType===USERS_TYPES.PROFESSIONALIST) && areas.length===0)
-                        || ((selectedType===USERS_TYPES.PROFESSIONALIST) && plan.length===0)
-                }
-                    onClick={() => {
-                        axios.get(HOST + "signup", {
-                            params: {selectedType, username, password,fullName, address, areas, plan,contactInfo}
-                        }).then(response => {
-                            if (response.data.success) {
-                                Cookies.set('token', response.data.token)
-                                navigate("/dashboard");
-                            } else {
-                                setSignupStatus(SIGNUP_STATUSES.FAILURE)
-                            }
-                        })
+                        username.length === 0 ||
+                        password.length !== 6 ||
+                        selectedType === USERS_TYPES.NONE ||
+                        contactInfo.length === 0 ||
+                        !fullName.includes(" ") ||
+                        (selectedType === USERS_TYPES.CLIENT &&
+                            address.length === 0) ||
+                        (selectedType === USERS_TYPES.PROFESSIONALIST &&
+                            areas.length === 0) ||
+                        (selectedType === USERS_TYPES.PROFESSIONALIST &&
+                            plan <= 0)
                     }
-                    }>
+                    onClick={() => {
+                        axios
+                            .get(HOST + "signup", {
+                                params: {
+                                    selectedType,
+                                    username,
+                                    password,
+                                    fullName,
+                                    address,
+                                    areas,
+                                    plan,
+                                    contactInfo
+                                }
+                            })
+                            .then((response) => {
+                                if (response.data.success) {
+                                    Cookies.set(
+                                        "token",
+                                        response.data.token
+                                    );
+                                    navigate("/dashboard");
+                                } else {
+                                    setSignupStatus(
+                                        SIGNUP_STATUSES.FAILURE
+                                    );
+                                }
+                            });
+                    }}
+                >
                     Sign Up
                 </button>
             </div>
@@ -132,14 +177,14 @@ function SignupPage() {
             <div>
                 <button
                     onClick={() => {
-                        navigate("/")
-                    }
-                    }>
+                        navigate("/");
+                    }}
+                >
                     Sign In
                 </button>
             </div>
         </>
-    )
+    );
 }
 
 export default SignupPage;
